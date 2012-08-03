0304b1775afa325b1ee689fe9f7d2d559bed5b8e
Chris <chris.gerke@gmail.com>
2012-08-02 19:02:12 -0700

    AppleGatekeeper
    Disable this for now.

---
+++ AppleGatekeeper.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#* AppleGatekeeper
+#+ chris.gerke@gmail.com
+#+
+#+ Description: Payload free. Disable Gatekeeper.
+#+
+#+ Version: 1.0
+#+
+#+ History:
+#+     1.0: Script.
+#+
+#+ TODO:
+#+     * Add error checking?
+
+ME=$0
+SCRIPT_DIR="$1/Contents/Resources"
+TARGET_DIR="$3"
+
+#+ // fix
+if [ -z "${TARGET_DIR}" ] || [ "${TARGET_DIR}" = "/" ]; then
+ TARGET_DIR=""
+fi
+
+#+ LaunchDaemon (daemon because softwareupdate requires root)
+sudo /usr/bin/defaults write "${TARGET_DIR}/Library/LaunchDaemons/GateKeeper" Label "com.chrisgerke.GateKeeper"
+sudo /usr/bin/defaults write "${TARGET_DIR}/Library/LaunchDaemons/GateKeeper" RunAtLoad -bool TRUE
+sudo /usr/bin/defaults write "${TARGET_DIR}/Library/LaunchDaemons/GateKeeper" ProgramArguments -array "/usr/sbin/spctl" "--master-disable"
+
+#+ or like this if you want to do it once
+#+ /usr/bin/defaults write /var/db/SystemPolicy-prefs enabled no
+
+#+ Permissions
+sudo /usr/sbin/chown root:wheel "${TARGET_DIR}/Library/LaunchDaemons/GateKeeper.plist"
+sudo /bin/chmod 644 "${TARGET_DIR}/Library/LaunchDaemons/GateKeeper.plist" 
+
+#+ Load Daemon
+sudo /bin/launchctl load -w "${TARGET_DIR}/Library/LaunchDaemons/GateKeeper.plist"
+
+exit 0
