ded2fdf4d4133311e178895b71a88501c093d7cc
Chris Gerke <chris.gerke@gmail.com>
2012-08-23 13:01:25 +1000

    cleaner

--- AppleUserTemplate.sh
+++ AppleUserTemplate.sh
@@ -17,9 +17,9 @@ ME=$0
 SCRIPT_DIR="$1/Contents/Resources"
 TARGET_DIR="$3"
 
-DESKTOP="Default.png"
-DESKTOP_DIR="/Library/ORG/Desktops"
-HOMEPAGE="http://INTRANET"
+DESKTOP="Licorice.peg.png"
+DESKTOP_DIR="/Library/RDA/FWDrop/Desktop/RDA"
+HOMEPAGE="http://intranet.rdigest.com"
 
 #+ // fix
 if [ -z "${TARGET_DIR}" ] || [ "${TARGET_DIR}" = "/" ]; then
@@ -33,8 +33,6 @@ if [ -r "${SCRIPT_DIR}/PAYLOAD" ]; then
  #+ Target
  sudo /bin/mkdir -p "${TARGET_DIR}/usr/local/bin"
  sudo /bin/mkdir -p "${TARGET_DIR}/${DESKTOP_DIR}"
- #+ Payload
- sudo /bin/cp -Rf "${SCRIPT_DIR}/PAYLOAD/dock" "${TARGET_DIR}/usr/local/bin/dock"
  #+ Payload
  sudo /bin/cp -Rf "${SCRIPT_DIR}/PAYLOAD/Desktops/" "${TARGET_DIR}/${DESKTOP_DIR}"
 fi
@@ -184,16 +182,6 @@ do
   sudo /usr/bin/defaults write "${TARGET_DIR}/System/Library/User Template/${USER_TEMPLATE}/Library/Preferences/.GlobalPreferences" com.apple.swipescrolldirection -bool NO
   sudo /usr/bin/defaults write "${TARGET_DIR}/System/Library/User Template/${USER_TEMPLATE}/Library/Preferences/.GlobalPreferences" com.apple.keyboard.fnState -bool YES
   
-  # maybe do this instead of eoprofile
-  #sudo /bin/rm -f "$HOME/Library/Preferences/com.apple.desktop.plist" > /dev/null 2>&1
-  #sudo /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.desktop.plist" -c 'Add Background:default:BackgroundColor array' > /dev/null 2>&1
-  #sudo /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.desktop.plist" -c 'Add Background:default:BackgroundColor:0 real 0'
-  #sudo /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.desktop.plist" -c 'Add Background:default:BackgroundColor:1 real 0'
-  #sudo /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.desktop.plist" -c 'Add Background:default:BackgroundColor:2 real 0'
-  #sudo /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.desktop.plist" -c 'Add Background:default:DrawBackgroundColor bool true'
-  #sudo /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.desktop.plist" -c 'Add Background:default:ImageFilePath string /System/Library/PreferencePanes/DesktopScreenEffectsPref.prefPane/Contents/Resources/DesktopPictures.prefPane/Contents/Resources/Transparent.png'
-  #sudo /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.desktop.plist" -c 'Add Background:default:NoImage bool true'
-
   #+ OK, so this is really ugly. I will work on making it nicer when I have time.
   sudo /bin/cat > "${TARGET_DIR}/System/Library/User Template/${USER_TEMPLATE}/Library/Preferences/com.apple.desktop.plist" << EOPROFILE
 <?xml version="1.0" encoding="UTF-8"?>
@@ -270,158 +258,6 @@ do
 </dict>
 </plist>
 
-EOPROFILE
-# End the ugliness
-
-  #+ OK, so this is really ugly but the only way to do it if you want to avoid supplying payloads items. I will work on making it nicer when I have time.
-  sudo /bin/cat > "${TARGET_DIR}/System/Library/User Template/${USER_TEMPLATE}/Library/Preferences/com.apple.dock.plist" << EOPROFILE
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>autohide</key>
-	<false/>
-	<key>launchanim</key>
-	<false/>
-	<key>mineffect</key>
-	<string>scale</string>
-	<key>mod-count</key>
-	<integer>6</integer>
-	<key>persistent-apps</key>
-	<array/>
-	<key>persistent-others</key>
-	<array>
-		<dict>
-			<key>GUID</key>
-			<integer>30450216</integer>
-			<key>tile-data</key>
-			<dict>
-				<key>arrangement</key>
-				<integer>1</integer>
-				<key>displayas</key>
-				<integer>1</integer>
-				<key>file-data</key>
-				<dict>
-					<key>_CFURLAliasData</key>
-					<data>
-					AAAAAAC2AAMAAQAAy2s50QAASCsAAAAAAAE+
-					BAABPgUAAMtrNbIAAAAACSD//gAAAAAAAAAA
-					/////wABAAwAAT4EAAE+AgABBpwADgAiABAA
-					UgBEAEEAIABBAHAAcABsAGkAYwBhAHQAaQBv
-					AG4AcwAPAA4ABgBTAHkAcwB0AGUAbQASACZM
-					aWJyYXJ5L1JEQS9Eb2NrSXRlbXMvUkRBIEFw
-					cGxpY2F0aW9ucwATAAEvAP//AAA=
-					</data>
-					<key>_CFURLString</key>
-					<string>file://localhost/Library/RDA/DockItems/RDA%20Applications/</string>
-					<key>_CFURLStringType</key>
-					<integer>15</integer>
-				</dict>
-				<key>file-label</key>
-				<string>RDA Applications</string>
-				<key>file-mod-date</key>
-				<integer>0</integer>
-				<key>file-type</key>
-				<integer>2</integer>
-				<key>parent-mod-date</key>
-				<integer>0</integer>
-				<key>preferreditemsize</key>
-				<integer>-1</integer>
-				<key>showas</key>
-				<integer>3</integer>
-			</dict>
-			<key>tile-type</key>
-			<string>directory-tile</string>
-		</dict>
-		<dict>
-			<key>GUID</key>
-			<integer>357144811</integer>
-			<key>tile-data</key>
-			<dict>
-				<key>arrangement</key>
-				<integer>1</integer>
-				<key>displayas</key>
-				<integer>1</integer>
-				<key>file-data</key>
-				<dict>
-					<key>_CFURLAliasData</key>
-					<data>
-					AAAAAACuAAMAAQAAy2s50QAASCsAAAAAAAE+
-					BAABPggAAMtp9qUAAAAACSD//gAAAAAAAAAA
-					/////wABAAwAAT4EAAE+AgABBpwADgAcAA0A
-					UgBEAEEAIABEAG8AYwB1AG0AZQBuAHQAcwAP
-					AA4ABgBTAHkAcwB0AGUAbQASACNMaWJyYXJ5
-					L1JEQS9Eb2NrSXRlbXMvUkRBIERvY3VtZW50
-					cwAAEwABLwD//wAA
-					</data>
-					<key>_CFURLString</key>
-					<string>file://localhost/Library/RDA/DockItems/RDA%20Documents/</string>
-					<key>_CFURLStringType</key>
-					<integer>15</integer>
-				</dict>
-				<key>file-label</key>
-				<string>RDA Documents</string>
-				<key>file-mod-date</key>
-				<integer>0</integer>
-				<key>file-type</key>
-				<integer>2</integer>
-				<key>parent-mod-date</key>
-				<integer>0</integer>
-				<key>preferreditemsize</key>
-				<integer>-1</integer>
-				<key>showas</key>
-				<integer>3</integer>
-			</dict>
-			<key>tile-type</key>
-			<string>directory-tile</string>
-		</dict>
-		<dict>
-			<key>GUID</key>
-			<integer>216516310</integer>
-			<key>tile-data</key>
-			<dict>
-				<key>arrangement</key>
-				<integer>1</integer>
-				<key>displayas</key>
-				<integer>1</integer>
-				<key>file-data</key>
-				<dict>
-					<key>_CFURLAliasData</key>
-					<data>
-					AAAAAACqAAMAAQAAy2s50QAASCsAAAAAAAE+
-					BAABPgoAAMtrNdcAAAAACSD//gAAAAAAAAAA
-					/////wABAAwAAT4EAAE+AgABBpwADgAaAAwA
-					UgBEAEEAIABIAGUAbABwAGQAZQBzAGsADwAO
-					AAYAUwB5AHMAdABlAG0AEgAiTGlicmFyeS9S
-					REEvRG9ja0l0ZW1zL1JEQSBIZWxwZGVzawAT
-					AAEvAP//AAA=
-					</data>
-					<key>_CFURLString</key>
-					<string>file://localhost/Library/RDA/DockItems/RDA%20Helpdesk/</string>
-					<key>_CFURLStringType</key>
-					<integer>15</integer>
-				</dict>
-				<key>file-label</key>
-				<string>RDA Helpdesk</string>
-				<key>file-mod-date</key>
-				<integer>0</integer>
-				<key>file-type</key>
-				<integer>2</integer>
-				<key>parent-mod-date</key>
-				<integer>0</integer>
-				<key>preferreditemsize</key>
-				<integer>-1</integer>
-				<key>showas</key>
-				<integer>3</integer>
-			</dict>
-			<key>tile-type</key>
-			<string>directory-tile</string>
-		</dict>
-	</array>
-	<key>version</key>
-	<integer>1</integer>
-</dict>
-</plist>
 EOPROFILE
 # End the ugliness
   
